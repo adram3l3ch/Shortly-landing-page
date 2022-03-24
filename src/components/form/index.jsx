aed7667bdeb4f8324624f7a6bedf1f86ccdebc87
adramelech <adramelech.psd@gmail.com>
2022-03-24 22:45:52 +0530

    stop yelling of eslint at regExp

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -11,6 +11,7 @@ const initialState = {
 };
 
 const regExp =
+	//eslint-disable-next-line
 	/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 
 const Form = ({ setLinks }) => {
