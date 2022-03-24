2d0634262fdf5d42f0d80c0787d3562f815c70bc
adramelech <adramelech.psd@gmail.com>
2022-03-24 22:36:42 +0530

    add floating nav for mobile devices

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../../images/logo.svg';
+import AsideNav from '../asideNav';
 import Button from '../button';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import './style.css';
 
 const Header = () => {
+	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	useEffect(() => {
+		isNavOpen
+			? (document.body.style.overflow = 'hidden')
+			: (document.body.style.overflow = 'auto');
+	}, [isNavOpen]);
+
 	return (
 		<header className='header'>
 			<div className='container'>
@@ -19,6 +29,13 @@ const Header = () => {
 						</li>
 					</ul>
 				</nav>
+				<GiHamburgerMenu
+					className='hamburger'
+					onClick={() => {
+						setIsNavOpen(!isNavOpen);
+					}}
+				/>
+				<AsideNav isNavOpen={isNavOpen} />
 			</div>
 		</header>
 	);
