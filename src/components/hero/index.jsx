117210ead0f62432fa9b2289cc59782829f8f750
adramelech <adramelech.psd@gmail.com>
2022-03-24 18:40:37 +0530

    style fix

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,25 +1,21 @@
 import Button from '../button';
 import Form from '../form';
-import Illustration from '../../images/illustration-working.svg';
 import './style.css';
 
 const Hero = () => {
 	return (
 		<main className='hero'>
-			<div className='container'>
-				<div className='intro'>
-					<div className='contents'>
-						<h1 className='hero__title'>More than just shorter links</h1>
-						<p className='hero__text'>
-							Build your brand's recognition and get detailed insights on how links
-							are performing.
-						</p>
-						<Button label='Get Started' classes='cta' />
-					</div>
-					<div className='image'>
-						<img src={Illustration} alt='' aria-hidden />
-					</div>
+			<div className='intro'>
+				<div className='container'>
+					<h1 className='hero__title'>More than just shorter links</h1>
+					<p className='hero__text'>
+						Build your brand's recognition and get detailed insights on how links are
+						performing.
+					</p>
+					<Button label='Get Started' classes='cta' />
 				</div>
+			</div>
+			<div className='container'>
 				<Form />
 			</div>
 		</main>
